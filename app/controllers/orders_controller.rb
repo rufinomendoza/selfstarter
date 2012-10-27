8dce9fae21f026f651090a33a4033e8d615daaca
rufinomendoza <ThinkPadT400@Mobile>
2012-10-27 02:12:03 -0400

    Removed an impertinent comment in orders_controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,6 @@
 class OrdersController < ApplicationController
   http_basic_authenticate_with :name => "admin", :password => "admin"
-  # GET /holdings
-  # GET /holdings.json
+
   def index
     @orders = Order.all
   end
